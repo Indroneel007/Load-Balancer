317f71eddd928e9d116fbc0f8186458624523998
Indroneel007 <2021ume1106@mnit.ac.in>
2025-11-04 20:56:14 +0530

    Least response time algorithm

--- internal/server/proxy_handlers.go
+++ internal/server/proxy_handlers.go
@@ -31,33 +31,75 @@ func NewProxy(target *url.URL) *httputil.ReverseProxy {
 func ProxyRequestHandler(lb *LoadBalancer, destinations []string, endpoint string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// choose next backend
-		idx := 0
-		if len(destinations) > 1 {
-			idx = lb.Next(len(destinations))
+		responseTimes := make(map[string]time.Duration)
+
+		if len(responseTimes) == 0 {
+			idx := lb.Next(len(destinations))
+			targetStr := destinations[idx]
+			targetURL, err := url.Parse(targetStr)
+			if err != nil {
+				http.Error(w, "bad backend url", http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Printf("Received request for %s, forwarding to %s\n", endpoint, targetURL.String())
+
+			// Update the headers to allow for SSL redirection
+			r.URL.Host = targetURL.Host
+			r.URL.Scheme = targetURL.Scheme
+			r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+			r.Host = targetURL.Host
+
+			// trim the endpoint prefix from the path
+			path := r.URL.Path
+			r.URL.Path = strings.TrimPrefix(path, endpoint)
+
+			fmt.Printf("[ TinyRP ] Redirecting request to %s at %s\n", r.URL, time.Now().UTC())
+
+			// Start a timer to measure response time
+			startTime := time.Now()
+			proxy := NewProxy(targetURL)
+			proxy.ServeHTTP(w, r)
+			responseTime := time.Since(startTime)
+
+			responseTimes[targetStr] = responseTime
+		} else {
+			// Find the backend with the lowest response time
+			minTime := time.Duration(0)
+			minBackend := ""
+			for backend, time := range responseTimes {
+				if time < minTime || minTime == 0 {
+					minTime = time
+					minBackend = backend
+				}
+			}
+
+			// Choose the backend with the lowest response time
+			targetStr := minBackend
+			targetURL, err := url.Parse(targetStr)
+			if err != nil {
+				http.Error(w, "bad backend url", http.StatusInternalServerError)
+				return
+			}
+
+			// Update the headers to allow for SSL redirection
+			r.URL.Host = targetURL.Host
+			r.URL.Scheme = targetURL.Scheme
+			r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+			r.Host = targetURL.Host
+
+			// trim the endpoint prefix from the path
+			path := r.URL.Path
+			r.URL.Path = strings.TrimPrefix(path, endpoint)
+
+			// Start a timer to measure response time
+			startTime := time.Now()
+			proxy := NewProxy(targetURL)
+			proxy.ServeHTTP(w, r)
+			responseTime := time.Since(startTime)
+
+			// Store the response time for this backend
+			responseTimes[targetStr] = responseTime
 		}
-		targetStr := destinations[idx]
-		targetURL, err := url.Parse(targetStr)
-		if err != nil {
-			http.Error(w, "bad backend url", http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Printf("Received request for %s, forwarding to %s\n", endpoint, targetURL.String())
-
-		proxy := NewProxy(targetURL)
-
-		// Update the headers to allow for SSL redirection
-		r.URL.Host = targetURL.Host
-		r.URL.Scheme = targetURL.Scheme
-		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
-		r.Host = targetURL.Host
-
-		// trim the endpoint prefix from the path
-		path := r.URL.Path
-		r.URL.Path = strings.TrimPrefix(path, endpoint)
-
-		// Note that ServeHTTP is non blocking and uses a goroutine under the hood
-		fmt.Printf("[ TinyRP ] Redirecting request to %s at %s\n", r.URL, time.Now().UTC())
-		proxy.ServeHTTP(w, r)
 	}
 }
